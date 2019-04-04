16a4f6af9464a6fa5930c076a6e54e460c01e841
clever-puppet-5071 <yoonmi.kim@rutgers.edu>
2019-04-04 21:08:44 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,19 @@
-class Student 
+require_relative 'user'
+
+class Student < User
+  
+  attr_accessor :knowledge
+  
+  def initialize 
+    @knowledge = []  
+  end
+
+  def learn(str) 
+    @knowledge << str
+  end
+
+  def knowledge
+    @knowledge
+  end
 
 end
